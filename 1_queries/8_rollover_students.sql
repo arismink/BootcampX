ccf50f4a4d6e2c0d7f2e8dbf120330aadd347c23
krismina la <krismina.la@gmail.com>
2022-04-05 03:48:37 +0000

    query for students that rolled over

---
+++ 1_queries/8_rollover_students.sql
@@ -0,0 +1,14 @@
+SELECT 
+  students.name AS student_name,
+  cohorts.name AS cohort_name,
+  students.start_date AS student_start_date,
+  cohorts.start_date AS cohort_start_date
+FROM
+  students
+JOIN 
+  cohorts ON cohorts.id = cohort_id
+WHERE
+  students.start_date != cohorts.start_date
+ORDER BY
+  cohorts.start_date
+;
