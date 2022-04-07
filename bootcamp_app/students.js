f9cf2176335f2e4afebb6158d4ad49ac58641af3
krismina la <krismina.la@gmail.com>
2022-04-07 03:48:44 +0000

    add code to prevent sql injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -10,7 +10,11 @@ const pool = new Pool({
 
 // user requested input
 const cohort_name = process.argv[2];
-const max_results = process.argv[3];
+const max_results = process.argv[3] || 5;
+
+// store potentially malicious values in array
+// in the query function, numbers start at 1 instead of 0
+const values = [`%${cohort_name}%`, max_results];
 
 // pool.query is a function that accepts SQL as a JS string
 // query returns a promise that contains a Result object which you can catch with .then()
@@ -22,9 +26,9 @@ pool.query(`
     cohorts.name as cohort
   FROM students
   JOIN cohorts ON students.cohort_id = cohorts.id
-  WHERE cohorts.name = $1
-  LIMIT ${max_results || 5};
-`, [cohort_name])
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+`, values)
 .then(res => { // so once this executes, we are not dealing with SQL or the database anymore but JS objects
   console.log(res.rows) // returns an object of many things but the thing we need is in rows
 
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,6 +9,7 @@ const pool = new Pool({
 });
 
 const cohort_name = process.argv[2];
+const values = [`%${cohort_name}%`];
 
 pool.query(`
   SELECT 
@@ -21,9 +22,9 @@ pool.query(`
     ON students.cohort_id = cohorts.id
   JOIN teachers
     ON teacher_id = teachers.id
-  WHERE cohorts.name = '${cohort_name}'
+  WHERE cohorts.name LIKE $1
   ORDER BY teacher;
-`)
+`, values)
 .then(res =>{
   res.rows.forEach(teacher => {
     console.log(`${teacher.cohort}: ${teacher.teacher}`);
